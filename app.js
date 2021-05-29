9b355ac6a508462bc49a28875c6a9f18779a0ece
agustinadm8 <agustinadm93@gmail.com>
2021-05-29 00:12:58 -0300

    heroku

--- app.js
+++ app.js
@@ -12,6 +12,6 @@ app.listen(app.get("port"), () => {console.log("Server start http://localhost"+a
 
 app.get("/",(req,res) => (res.sendFile(path.resolve(__dirname,"./views/home.html"))))
 
-/*app.get("/register.html",(req,res) => (res.sendFile(path.resolve(__dirname,"./views/register.html"))))
+app.get("/register.html",(req,res) => (res.sendFile(path.resolve(__dirname,"./views/register.html"))))
 
-app.get("/login.html",(req,res) => (res.sendFile(path.resolve(__dirname,"./views/login.html"))))*/
+app.get("/login.html",(req,res) => (res.sendFile(path.resolve(__dirname,"./views/login.html"))))
